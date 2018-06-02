fd64d5e89d4bc3d0ab85d8026f169ca8243e96e6
nfour <novus.nfour@gmail.com>
2018-06-02 21:15:58 +1000

    wip

--- src/variables.ts
+++ src/variables.ts
@@ -12,26 +12,29 @@ export const colors = {
 
   red: '#CF1C58',
   orange: '#cd772c',
-  yellow: '#BBD900',
+  yellow: '#CFD175',
   gold: '#bfa383',
-  grass: '#BBD900',
+  grass: '#A2BF51',
   lime: '#009D4D',
-  teal: '#3BC6AE',
-  cyan: '#009BA9',
-  blue: '#311F76',
-  purple: '#954EB8',
-  darkPurple: '#31273F',
+  teal: '#5C9D8E',
+  cyan: '#0BBFCF',
+  blue: '#4E4AD6',
+  purple: '#9C52C1',
+  darkPurple: '#705693',
 
-  shade: '#9f9f9f',
-  shade1: '#949494',
-  shade2: '#727272',
-  shade3: '#4a4a4a',
+  shade: '#808080',
+  shade1: '#747474',
+  shade2: '#646464',
+  shade3: '#3a3a3a',
 };
 
 export const elements = {
-  importantText: colors.foregrounds[0],
+  importantText: C(colors.cyan).lighten(0.5).desaturate(0.5).hex(),
   text: colors.foregrounds[1],
-  background: C(colors.back).mix(C(colors.red).darken(0.5)).hex(),
+  background: C(colors.back)
+    .mix(C(colors.blue))
+    .mix(C(colors.purple).desaturate(0.4))
+    .darken(0.6).desaturate(0.34).hex(),
 
   // Not sure where these get used
   bracketsForeground: colors.red,
@@ -47,27 +50,27 @@ export const elements = {
   delimiter: colors.shade2,
   brace: colors.shade2,
   block: colors.shade3,
-  string: colors.cyan,
+  string: C(colors.grass).lighten(0.2).desaturate(0.1).hex(),
   regexp: colors.blue,
   quote: colors.shade1,
-  comment: colors.purple,
-  annotation: C(colors.darkPurple).saturate(0.6).hex(),
-  annotation2: C(colors.darkPurple).desaturate(0.35).lighten(0.15).hex(),
-  annotation3: C(colors.darkPurple).mix(C(colors.teal), 0.5).desaturate(0.34).darken(0.12).hex(),
+  comment: C(colors.blue).lighten(0.2).desaturate(0.4).hex(),
+  annotation: C(colors.blue).desaturate(0.2).hex(),
+  annotation2: C(colors.blue).desaturate(0.45).lighten(0.15).hex(),
+  annotation3: C(colors.blue).mix(C(colors.teal), 0.5).desaturate(0.44).darken(0.12).hex(),
 
-  function: colors.orange,
+  function: C(colors.purple).lighten(0.3).hex(),
 
-  variable: colors.yellow,
-  support: C(colors.yellow).mix(C(colors.foregrounds[1])).hex(),
+  variable: colors.cyan,
+  support: C(colors.lime).mix(C(colors.foregrounds[1])).hex(),
   supportType: colors.red,
   supportFunction: colors.lime,
   storage: colors.shade,
   storage2: colors.shade1,
-  entity: colors.shade,
-  constant: colors.teal,
-  class: colors.teal,
+  entity: colors.red,
+  constant: C(colors.teal).mix(C(colors.cyan).desaturate(0.5)).hex(),
+  class: colors.cyan,
 
-  propWrite: colors.yellow,
+  propWrite: colors.teal,
   propRead: colors.shade,
 
   caret: '#ffffff',
